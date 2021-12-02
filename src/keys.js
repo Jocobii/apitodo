4b35215844df425640bd9dbb4fe59aee1328257e
Alexander Jocobi <vazquez_alex@outlook.com>
2021-12-01 18:39:51 -0800

    init

---
+++ src/keys.js
@@ -0,0 +1,16 @@
+// exportamos credenciales
+
+module.exports = {
+    database: {
+        host: 'b5hshyrhz7v2zko6hgq9-mysql.services.clever-cloud.com',
+        user: 'uo3aechqugdlzwb9',
+        password: 'YDuwY2Qtl1vmoRRlC286',
+        database: 'b5hshyrhz7v2zko6hgq9'
+    }
+    //database: {
+    //    host: 'localhost',
+    //    user: 'root',
+    //    password: 'gglobal',
+    //    database: 'mytodos'
+    //}
+};
